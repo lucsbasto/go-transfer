66657331894ab49c1597f8b4f1c6ba340b359a5f
Lucas Bastos <lucsbasto@gmail.com>
2025-04-07 19:01:24 -0300

    refactor: group wallet_repository_inmemory tests with implementation

--- internal/infra/repositories/wallet_repository_inmemory_test.go
+++ internal/infra/repositories/wallet_repository_inmemory_test.go
@@ -2,12 +2,71 @@ package repositories
 
 import (
 	"context"
+	"errors"
+	"go-transfer/internal/domain/port"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"go-transfer/internal/domain/entities"
 )
+
+type WalletRepositoryInMemory struct {
+	wallets map[int64]*entities.Wallet
+	mu      sync.RWMutex
+	nextID  int64
+}
+
+func NewWalletRepositoryInMemory() port.WalletRepository {
+	return &WalletRepositoryInMemory{
+		wallets: make(map[int64]*entities.Wallet),
+		mu:      sync.RWMutex{},
+		nextID:  1,
+	}
+}
+
+func (r *WalletRepositoryInMemory) Create(ctx context.Context, wallet *entities.Wallet) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	wallet.ID = r.nextID
+	r.wallets[wallet.ID] = wallet
+	r.nextID++
+	return nil
+}
+
+func (r *WalletRepositoryInMemory) GetByID(ctx context.Context, id int64) (*entities.Wallet, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	wallet, ok := r.wallets[id]
+	if !ok {
+		return nil, errors.New("carteira não encontrada")
+	}
+	return wallet, nil
+}
+
+func (r *WalletRepositoryInMemory) GetByOwnerID(ctx context.Context, ownerID int64) (*entities.Wallet, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	for _, wallet := range r.wallets {
+		if wallet.OwnerID == ownerID {
+			return wallet, nil
+		}
+	}
+	return nil, errors.New("carteira não encontrada para o OwnerID")
+}
+
+func (r *WalletRepositoryInMemory) UpdateBalance(ctx context.Context, id int64, balance float64) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	wallet, ok := r.wallets[id]
+	if !ok {
+		return errors.New("carteira não encontrada")
+	}
+	wallet.Balance = balance
+	wallet.UpdatedAt = time.Now()
+	return nil
+}
 
 func TestWalletRepositoryInMemory_Create(t *testing.T) {
 	repo := NewWalletRepositoryInMemory()
