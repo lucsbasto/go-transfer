11592fb53b5cce2fe8e4bffbafd1f7bf8196162d
Lucas Bastos <lucsbasto@gmail.com>
2025-04-07 19:47:42 -0300

    feat: add transaction repository GetById

--- internal/domain/port/transaction_repository.go
+++ internal/domain/port/transaction_repository.go
@@ -9,4 +9,5 @@ import (
 type TransactionRepository interface {
 	Create(ctx context.Context, transfer *entities.Transaction) (int64, error)
 	UpdateStatus(ctx context.Context, id int64, status entities.TransactionStatus) error
+	GetByID(ctx context.Context, id int64) (*entities.TransactionStatus, error)
 }
--- internal/infra/repositories/transaction_repository.go
+++ internal/infra/repositories/transaction_repository.go
@@ -29,3 +29,12 @@ func (r *TransactionRepository) Create(ctx context.Context, transfer *entities.T
 func (r *TransactionRepository) UpdateStatus(ctx context.Context, id int64, status entities.TransactionStatus) error {
 	return r.db.WithContext(ctx).Model(&entities.Transaction{}).Where("id = ?", id).Update("status", status).Error
 }
+
+func (r *TransactionRepository) GetByID(ctx context.Context, id int64) (*entities.TransactionStatus, error) {
+	transaction := &entities.Transaction{}
+	err := r.db.WithContext(ctx).First(transaction, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &transaction.Status, nil
+}
